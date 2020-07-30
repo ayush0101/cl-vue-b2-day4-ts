c5b2abb4cdde572e8dc279345f62efbb85d8f0a6
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-30 22:21:09 +0530

    done

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,20 @@
   <div id="app">
     <div id="nav">
       <h3>Learning TypeScript Tooling</h3>
+      <Playground />
     </div>
   </div>
 </template>
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
+import Playground from "./components/Playground.vue";
 
-@Component
+@Component({
+  components: {
+    Playground,
+  },
+})
 export default class App extends Vue {}
 </script>
 <style>
