b0af4078383a4cd21b90eebc8bf5e5917fb8ec20
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-30 22:12:13 +0530

    done

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,12 @@
     </div>
   </div>
 </template>
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
 
+@Component
+export default class App extends Vue {}
+</script>
 <style>
 </style>
---
+++ src/components/Playground.vue
@@ -0,0 +1,14 @@
+<template>
+  <h3>This is Playground</h3>
+</template>
+
+<script>
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class Playground extends Vue {}
+</script>
+
+<style>
+</style>
