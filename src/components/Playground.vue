be2099876ec18eba01d18334a3ac2bb1ab234893
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-30 22:25:22 +0530

    done

--- src/components/Playground.vue
+++ src/components/Playground.vue
@@ -3,6 +3,9 @@
     <h3>Welcome to counter game!</h3>
     <hr />
     <h4>Counter: {{counter}}</h4>
+    <button @click="clickHandler('+')">Increment</button>
+    <br />
+    <button @click="clickHandler('-')">Decrement</button>
   </div>
 </template>
 
@@ -15,6 +18,10 @@ import { Prop } from "vue-property-decorator";
 export default class Playground extends Vue {
   //  Declaring a prop
   @Prop() counter!: number;
+
+  clickHandler(arg: string): void {
+    console.log("Handler called", arg);
+  }
 }
 </script>
 
