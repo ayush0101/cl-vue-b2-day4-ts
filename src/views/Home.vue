ddc938ac69f2da42ece1cd5f00bfeb00a73c50bd
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-30 22:02:10 +0530

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" count="120" @custom-event="parentHandler" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+  methods: {
+    parentHandler: function (e) {
+      console.log("Parent", e);
+    },
+  },
+};
+</script>
